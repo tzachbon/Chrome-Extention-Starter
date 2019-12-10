88809c0555ff696b339fda14022d4eaf8fcb67db
Tzach Bonfil <tzachbonfil@gmail.com>
2019-12-10 10:49:52 +0200

    adding material ui

--- src/popup/containers/Popup/Popup.tsx
+++ src/popup/containers/Popup/Popup.tsx
@@ -1,6 +1,42 @@
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import { observer, useLocalStore } from 'mobx-react';
 import * as React from 'react';
 import './Popup.scss';
+
+
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        grey: {
+            800: "#000000", // overrides failed
+            900: "#121212" // overrides success
+        },
+        background: {
+            paper: "#000000"
+        },
+        primary: {
+            light: '#fff',
+            main: '#09d3ac',
+            dark: '#eee',
+            contrastText: '#eee'
+        },
+        secondary: {
+            main: '#40739e',
+            dark: '#eee',
+            light: '#fff',
+            contrastText: '#eee'
+
+        },
+    },
+    typography: {
+        fontFamily: "Nunito Sans, Roboto, sans-serif",
+        fontSize: 20,
+        allVariants: {
+            color: '#eee'
+        }
+    },
+});
 
 
 interface AppProps { }
@@ -8,13 +44,17 @@ interface AppProps { }
 interface AppState { }
 
 const App: React.FC = (props: AppProps) => {
-    const state = useLocalStore(() => ({  }));
+    const state = useLocalStore(() => ({}));
 
 
     return (
-        <div className="popupContainer" >
-            Hello World!
-        </div>
+        <MuiThemeProvider theme={theme}>
+            <div className="popupContainer" >
+                <Typography variant="h3" className='flex-center t-center'  gutterBottom>
+                    Hello World!
+                </Typography>
+            </div>
+        </MuiThemeProvider>
     )
 }
 
